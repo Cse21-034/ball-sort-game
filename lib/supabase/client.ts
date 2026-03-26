3ad5244a056e2f4c0c9d8a764e0a9b9d534ba5a0
leatile mosimanyana <100219407+Cse21-034@users.noreply.github.com>
2026-03-26 13:08:03 +0200

    Update client.ts

--- lib/supabase/client.ts
+++ lib/supabase/client.ts
@@ -8,8 +8,8 @@ export function createClient() {
   }
 
   supabaseInstance = createBrowserClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    process.env.NEXT_PUBLIC_NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_NEXT_PUBLIC_SUPABASE_ANON_KEY!,
   )
 
   return supabaseInstance
